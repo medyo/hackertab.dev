965827af22dd2af1f125817ffcb42678479cf411
Mehdi Sakout <elmehdi.sakout@gmail.com>
2021-03-16 15:47:15 +0100

    move the source code link to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { APP } from '../Constants';
+import { RiCodeSSlashFill } from "react-icons/ri"
 
 function Footer({ setCurrentPage }) {
 
+  const onSourceCodeClick = () => {
+    window.open(APP.repository, "_blank")
+  }
+
   return (
     <footer className="AppFooter">
+      <a className="linkItem" href="#" onClick={() => onSourceCodeClick()}><RiCodeSSlashFill className="linkItemIcon" /> Source code</a>
       <a className="linkItem" href="#" onClick={() => setCurrentPage('terms')}>{`Terms & conditions`}</a>
       <a className="linkItem" href="#" onClick={() => setCurrentPage('privacy')}>Privacy policy</a>
       <a className="linkItem" href="#" onClick={() => setCurrentPage('dataSource')}>Data sources</a>
