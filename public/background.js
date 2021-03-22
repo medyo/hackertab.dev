97d93eee840dad8fd7e3dc054276acb8a7332b4d
Mehdi Sakout <elmehdi.sakout@gmail.com>
2021-03-22 16:46:16 +0100

    ask about users feedback when uninstalling the extension

--- public/background.js
+++ public/background.js
@@ -2,3 +2,8 @@
   chrome.tabs.create({ url: "chrome://newtab" });
 }
 );*/
+
+const uninstallUrl = `https://hackertab.dev/uninstall.html`;
+if (chrome.runtime.setUninstallURL) {
+  chrome.runtime.setUninstallURL(uninstallUrl);
+}
