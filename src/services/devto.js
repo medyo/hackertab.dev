49829dd5cd81796d97fbde4593921e787648ef3d
Chouikane <chouikaneabdelilah@gmail.com>
2021-03-25 20:39:21 +0100

    get the day top articles

--- src/services/devto.js
+++ src/services/devto.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const getArticles = async (tag) => {
-    const url = `https://dev.to/api/articles?state=rising&per_page=30&tag=${tag}`
+    const url = `https://dev.to/api/articles?top=1&per_page=30&tag=${tag}`
     let { data } =  await axios.get(url)
     return data
 }
