e924d0ca22a25a1a21eac19432ffbdb544e39c6e
medyo <elmehdi.sakout@gmail.com>
2021-06-13 12:24:01 +0100

    remove caching using etag (mot well supported by netlify)

--- src/services/cachedRequest.js
+++ src/services/cachedRequest.js
@@ -10,8 +10,6 @@ const forageStore = localforage.createInstance({
   name: 'hackertab-cache'
 })
 
-
-
 const api = setup({
   cache: {
     maxAge: 15 * 60 * 1000,
@@ -30,39 +28,5 @@ const cachedRequest = async (url, maxAge = 15) => {
 
   return data
 }
-
-/*const cachedRequest = async (url) => {
-    const cachedResponse = await AppStorage.getCachedResponse(url)
-    let headers = {}
-    let response
-
-    if (cachedResponse) {
-        headers = {
-            "If-None-Match": cachedResponse.etag,
-        }
-    }
-
-    
-    try {
-        response = await axios.get(url, { headers })
-        if (response.headers.etag) {
-            AppStorage.cacheResponse(url, response)
-        }
-
-    } catch (error) {
-        if (!error.response || error.response.status !== 304) {
-            throw error
-        }
-        if (!cachedResponse) {
-            throw "Network Failed"
-        }
-        response = error.response;
-        response.status = 200;
-        response.data = cachedResponse.data;
-    }
-
-    return response.data
-}*/
-
 
 export default cachedRequest
