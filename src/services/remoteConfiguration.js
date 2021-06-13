dffc2df6f145576b1bbc95bc59f7007d41fd0c6a
medyo <elmehdi.sakout@gmail.com>
2021-06-13 12:21:46 +0100

    fix maxAge param

--- src/services/remoteConfiguration.js
+++ src/services/remoteConfiguration.js
@@ -1,9 +1,9 @@
 import cachedRequest from './cachedRequest';
 
 const getRemoteConfiguration = async () => {
-  const url = `/data/remoteConfiguration.json`;
-  const data = await cachedRequest(url, maxAge = 60);
-  return data;
+  const url = `/data/remoteConfiguration.json`
+  const data = await cachedRequest(url, 60)
+  return data
 }
 
 export default {
