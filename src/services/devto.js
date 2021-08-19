a9a1602599228f8887890d77722e65a34605131f
Mehdi Sakout <elmehdi.sakout@gmail.com>
2021-08-19 17:04:19 +0100

    serve devto and reddit from the hackertab api

--- src/services/devto.js
+++ src/services/devto.js
@@ -2,7 +2,7 @@ import cachedRequest from './cachedRequest';
 
 
 const getArticles = async (tag) => {
-  const url = `https://dev.to/api/articles?state=rising&per_page=30&tag=${tag}`;
+  const url = `/devto/articles?state=rising&per_page=30&tag=${tag}`;
   const data = await cachedRequest(url);
   return data;
 };
