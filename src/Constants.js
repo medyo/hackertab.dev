74ebfa9d53867bc88bd3072522d692ad559f71ca
Mehdi Sakout <elmehdi.sakout@gmail.com>
2021-09-10 19:16:41 +0100

    add lobsters source

--- src/Constants.js
+++ src/Constants.js
@@ -5,31 +5,42 @@ import JobsCard from "./cards/JobsCard";
 import ReposCard from "./cards/ReposCard";
 import ProductHuntCard from "./cards/ProductHuntCard";
 import RedditCard from "./cards/RedditCard";
+import LobstersCard from './cards/LobstersCard'
 
 const APP = {
-	name: 'Hackertab.dev',
-	slogan: '— Stay updated with the new technology and trends',
-	repository: 'https://github.com/medyo/hackertab.dev',
-	ref: 'utm_source=hackertab.dev&utm_medium=post&utm_campaign=home',
-	contactEmail: 'hello@hackertab.dev',
-	maxCardsPerRow: 4,
-	donationLink: 'https://www.buymeacoffee.com/medyo',
-	supportLink: 'https://github.com/medyo/hackertab.dev/issues',
-};
-
-export const LOCAL_CONFIGURATION = {
-  supportedTags: [] // Loaded remotly
+  name: 'Hackertab.dev',
+  slogan: '— Stay updated with the new technology and trends',
+  repository: 'https://github.com/medyo/hackertab.dev',
+  ref: 'utm_source=hackertab.dev&utm_medium=post&utm_campaign=home',
+  contactEmail: 'hello@hackertab.dev',
+  maxCardsPerRow: 4,
+  donationLink: 'https://www.buymeacoffee.com/medyo',
+  supportLink: 'https://github.com/medyo/hackertab.dev/issues',
 }
 
+export const LOCAL_CONFIGURATION = {
+  supportedTags: [], // Loaded remotly
+}
 
 export const SUPPORTED_CARDS = [
   { value: 'github', analyticsTag: 'repos', label: 'Github repositories', component: ReposCard },
   { value: 'jobs', analyticsTag: 'jobs', label: 'Featured jobs', component: JobsCard },
   { value: 'hackernews', analyticsTag: 'hackernews', label: 'Hackernews', component: HNCard },
-  { value: 'conferences', analyticsTag: 'events', label: 'Upcoming events', component: ConferencesCard },
+  {
+    value: 'conferences',
+    analyticsTag: 'events',
+    label: 'Upcoming events',
+    component: ConferencesCard,
+  },
   { value: 'devto', analyticsTag: 'devto', label: 'DevTo', component: DevToCard },
-  { value: 'producthunt', analyticsTag: 'producthunt', label: 'Product Hunt', component: ProductHuntCard },
+  {
+    value: 'producthunt',
+    analyticsTag: 'producthunt',
+    label: 'Product Hunt',
+    component: ProductHuntCard,
+  },
   { value: 'reddit', analyticsTag: 'reddit', label: 'Reddit', component: RedditCard },
+  { value: 'lobsters', analyticsTag: 'lobsters', label: 'Lobsters', component: LobstersCard },
 ]
 
 export const LS_PREFERENCES_KEY = "hackerTabPrefs"
---
+++ src/cards/LobstersCard.js
@@ -0,0 +1,80 @@
+import React, { useContext } from 'react'
+import { format } from 'timeago.js'
+import { SiLetterboxd } from 'react-icons/si'
+import { VscTriangleUp } from 'react-icons/vsc'
+import CardComponent from '../components/CardComponent'
+import ListComponent from '../components/ListComponent'
+import lobstersApi from '../services/lobsters'
+import { BiCommentDetail } from 'react-icons/bi'
+import { MdAccessTime } from 'react-icons/md'
+import { GoPrimitiveDot } from 'react-icons/go'
+import CardLink from '../components/CardLink'
+import CardItemWithActions from '../components/CardItemWithActions'
+import ClickableItem from '../components/ClickableItem'
+import PreferencesContext from '../preferences/PreferencesContext'
+
+const StoryItem = ({ item, index, analyticsTag }) => {
+  const { listingMode } = useContext(PreferencesContext)
+
+  return (
+    <CardItemWithActions
+      source={'lobsters'}
+      index={index}
+      item={item}
+      key={index}
+      cardItem={
+        <>
+          <p className="rowTitle">
+            <CardLink link={item.url} analyticsSource={analyticsTag}>
+              {listingMode === 'compact' && (
+                <div className="counterWrapper">
+                  <VscTriangleUp />
+                  <span className="value">{item.score}</span>
+                </div>
+              )}
+
+              <div className="subTitle">{item.title}</div>
+            </CardLink>
+          </p>
+          {listingMode === 'normal' && (
+            <div className="rowDetails">
+              <span className="rowItem lobstersRowItem">
+                <GoPrimitiveDot className="rowItemIcon" /> {item.score} points
+              </span>
+              <span className="rowItem" title={new Date(item.created_at).toUTCString()}>
+                <MdAccessTime className="rowItemIcon" /> {format(new Date(item.created_at))}
+              </span>
+              <ClickableItem
+                link={item.comments_url}
+                className="rowItem rowItemClickable"
+                analyticsSource={analyticsTag}>
+                <BiCommentDetail className="rowItemIcon" /> {item.comment_count} comments
+              </ClickableItem>
+            </div>
+          )}
+        </>
+      }
+    />
+  )
+}
+
+function LobstersCard({ analyticsTag, label, withAds }) {
+  const fetchStories = async () => {
+    return await lobstersApi.getTopStories()
+  }
+
+  const renderItem = (item, index) => (
+    <StoryItem item={item} key={`lb-${index}`} index={index} analyticsTag={analyticsTag} />
+  )
+
+  return (
+    <CardComponent
+      icon={<SiLetterboxd className="blockHeaderIcon" color="#A91916" />}
+      link="https://lobste.rs/"
+      title={label}>
+      <ListComponent fetchData={fetchStories} renderItem={renderItem} withAds={withAds} />
+    </CardComponent>
+  )
+}
+
+export default LobstersCard
