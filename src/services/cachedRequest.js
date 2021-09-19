57a5427e7cec0dcb8bc55736b96a1e73d37593d0
medyo <elmehdi.sakout@gmail.com>
2021-09-20 00:11:01 +0100

    skip setting etag on web version (cors issue)

--- src/services/cachedRequest.js
+++ src/services/cachedRequest.js
@@ -6,35 +6,36 @@ var packageFile = require("../../package.json");
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === "production" ? packageFile.proxy : null,
 });
+const isWebVersion = !!+process.env.REACT_APP_WEB_BUILD
 
 const cachedRequest = async (url) => {
-  const cachedResponse = await AppStorage.getItem(url);
-  let headers = {};
-  let response;
-  if (cachedResponse) {
-    let etag = JSON.parse(cachedResponse).headers.etag;
+  const cachedResponse = await AppStorage.getItem(url)
+  let headers = {}
+  let response
+  if (!isWebVersion && cachedResponse) {
+    let etag = JSON.parse(cachedResponse).headers.etag
     headers = {
-      "If-None-Match": etag,
-    };
+      'If-None-Match': etag,
+    }
   }
 
   try {
-    response = await axiosInstance.get(url, { headers });
+    response = await axiosInstance.get(url, { headers })
     if (response.headers.etag) {
-      AppStorage.setItem(url, response);
+      AppStorage.setItem(url, response)
     }
   } catch (error) {
     if (!error.response || error.response.status !== 304) {
-      throw error;
+      throw error
     }
     if (!cachedResponse) {
-      throw "Network Failed";
+      throw 'Network Failed'
     }
-    response = error.response;
-    response.status = 200;
-    response.data = JSON.parse(cachedResponse).data;
+    response = error.response
+    response.status = 200
+    response.data = JSON.parse(cachedResponse).data
   }
-  return response.data;
-};
+  return response.data
+}
 
 export default cachedRequest
