2abf8f5e7d9d430583f3a3c78e5182c1b2371551
medyo <elmehdi.sakout@gmail.com>
2021-10-28 23:53:16 +0100

    calculate card width based on view width

--- src/components/ScrollCardsNavigator.js
+++ src/components/ScrollCardsNavigator.js
@@ -2,6 +2,10 @@ import React, { useState, useReducer, useEffect, useContext, useRef } from 'reac
 import { TiChevronRight, TiChevronLeft } from 'react-icons/ti'
 
 function ScrollCardsNavigator() {
+  const [leftButtonVisible, setLeftButtonVisible] = useState(true)
+  const [rightButtonVisible, setRightButtonVisible] = useState(true)
+
+  useEffect(() => {}, [])
   const scrollTo = (direction) => {
     const scrollContainer = document.querySelector('.AppContent')
     if (!scrollContainer) {
@@ -21,12 +25,16 @@ function ScrollCardsNavigator() {
 
   return (
     <>
-      <button onClick={() => scrollTo('left')} className="scrollButton" style={{ left: 0 }}>
-        <TiChevronLeft size={32} />
-      </button>
-      <button onClick={() => scrollTo('right')} className="scrollButton" style={{ right: 0 }}>
-        <TiChevronRight size={32} />
-      </button>
+      {leftButtonVisible && (
+        <button onClick={() => scrollTo('left')} className="scrollButton" style={{ left: 0 }}>
+          <TiChevronLeft size={32} />
+        </button>
+      )}
+      {rightButtonVisible && (
+        <button onClick={() => scrollTo('right')} className="scrollButton" style={{ right: 0 }}>
+          <TiChevronRight size={32} />
+        </button>
+      )}
     </>
   )
 }
