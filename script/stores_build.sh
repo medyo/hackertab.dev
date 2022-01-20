8a76df1fc0fc4a8bea1d61fd483bc36dfdbab120
medyo <elmehdi.sakout@gmail.com>
2022-01-20 21:08:43 +0100

    build extension for each store

--- script/stores_build.sh
+++ script/stores_build.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-stores_build() {
-    echo 'building app for Chrome & Firefox Stores'
+chrome_build() {
+    echo 'building app for Chrome'
 
     rm -rf dist/*
+    rm -f chrome_extension.zip
+
+    export INLINE_RUNTIME_CHUNK=false
+    export GENERATE_SOURCEMAP=false
+    
+    yarn build
+
+    mkdir -p dist
+    cp -r build/* dist
+    echo 'zipping the dist'
+    cd dist/ && zip -r ../chrome_extension.zip * -x "*.DS_Store" && cd ..
+}
+
+firefox_build() {
+    echo 'building app for Firefox'
+
+    rm -rf dist/*
+    rm -f source_code.zip
+    rm -f firefox_extension.zip
 
     export INLINE_RUNTIME_CHUNK=false
     export GENERATE_SOURCEMAP=false
 
-    rm -f source_code.zip
-    rm -f extension.zip
-    
     yarn global add @craco/craco
     craco build
 
     mkdir -p dist
     cp -r build/* dist
     echo 'zipping the dist'
-    cd dist/ && zip -r ../extension.zip * -x "*.DS_Store" && cd ..
+    cd dist/ && zip -r ../firefox_extension.zip * -x "*.DS_Store" && cd ..
 
     echo 'zipping the source code for Firefox'
     zip -r source_code.zip 'public/' 'script/' 'src' 'LICENSE' 'package.json' 'yarn.lock' 'README.md' 'craco.config.js' '.env' -x "*.DS_Store"
 }
-
-stores_build
+chrome_build
+firefox_build
