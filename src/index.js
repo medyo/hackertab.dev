fc59bda88f33c302a192fcd80370836dd51c485c
medyo <elmehdi.sakout@gmail.com>
2022-10-23 15:21:46 +0100

    add the app using QueryClientProvider

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import App from './App';
 import AppWrapper from './configuration/AppWrapper'
 import AppErrorBoundary from './configuration/AppErrorBoundary'
 import ConfigurationWrapper from './configuration/ConfigurationWrapper'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from 'src/lib/react-query'
 
 ReactDOM.render(
   <React.StrictMode>
     <AppErrorBoundary>
-      <ConfigurationWrapper>
-        <AppWrapper>
-          <App />
-        </AppWrapper>
-      </ConfigurationWrapper>
+      <QueryClientProvider client={queryClient}>
+        <ConfigurationWrapper>
+          <AppWrapper>
+            <App />
+          </AppWrapper>
+        </ConfigurationWrapper>
+      </QueryClientProvider>
     </AppErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
