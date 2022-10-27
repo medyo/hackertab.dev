9a1cdb9e75e832e70da8e755d21ecad8efedb215
medyo <elmehdi.sakout@gmail.com>
2022-10-27 20:22:23 +0100

    remove debug line

--- src/features/changelog/api/getVersions.ts
+++ src/features/changelog/api/getVersions.ts
@@ -8,7 +8,6 @@ const getAd = async (): Promise<Version[]> => {
   return axios.get<Version[]>('https://api.github.com/repos/medyo/hackertab.dev/releases')
   .then(response => {
     const versions = response.data as unknown as Version[];
-    console.log("response.data", versions)
     return versions
   })
 }
