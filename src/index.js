eb5ae0d52065fda9eac372fb870b34b8bb1e3525
medyo <elmehdi.sakout@gmail.com>
2022-10-28 14:33:57 +0100

    move wrappers to providers folder

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import App from './App';
-import AppWrapper from './configuration/AppWrapper'
-import AppErrorBoundary from './configuration/AppErrorBoundary'
+import AppWrapper from 'src/providers/AppWrapper'
+import AppErrorBoundary from 'src/providers/AppErrorBoundary'
 import { ConfigurationWrapper } from 'src/features/remoteConfig/'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from 'src/lib/react-query'
