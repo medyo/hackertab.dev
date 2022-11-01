ac0216183977747fc1e0bc311fb2b8860248b69d
Chouikane <chouikaneabdelilah@gmail.com>
2022-11-01 15:09:41 +0100

    producthunt cards

---
+++ src/features/producthuntCard/components/RedditCard.tsx
@@ -0,0 +1,35 @@
+import CardComponent from '../../../components/CardComponent'
+import { ListComponent } from 'src/components/List'
+import { useGetArticles } from '../api/getArticles'
+import { ArticleType, CardPropsType } from 'src/types'
+import { ProductHuntPlaceholder } from 'src/components/placeholders'
+import ArticleItem from './ArticleItem';
+
+
+
+export function RedditCard(props: CardPropsType) {
+  const { label, icon, withAds } = props
+  const { data: articles = [], isLoading, error } = useGetArticles()
+
+  const renderItem = (item: ArticleType, index: number) => (
+    <ArticleItem item={item} key={`ph-${index}`} index={index}  />
+  )
+
+  return (
+    <CardComponent
+      icon={<span className="blockHeaderIcon">{icon}</span>}
+      link="https://producthunt.com/"
+      title={label}>
+      <ListComponent
+        items={articles}
+        error={error}
+        isLoading={isLoading}
+        renderItem={renderItem}
+        withAds={withAds}
+        placeholder={<ProductHuntPlaceholder />}
+      />
+    </CardComponent>
+  )
+}
+
+
---
+++ src/features/redditCard/components/ArticleItem.tsx
@@ -0,0 +1,59 @@
+
+import { BiCommentDetail } from 'react-icons/bi'
+import { VscTriangleUp } from 'react-icons/vsc'
+import CardLink from 'src/components/CardLink'
+import CardItemWithActions from 'src/components/CardItemWithActions'
+import { Attributes } from 'src/lib/analytics'
+import { ArticleItemPropsType } from 'src/types'
+
+
+const ArticleItem = (props: ArticleItemPropsType) => {
+  const { item, index, listingMode } = props
+
+  return (
+    <CardItemWithActions
+      source={'producthunt'}
+      index={index}
+      key={index}
+      item={{ ...item, title: item.title }}
+      cardItem={
+        <div className="phItem">
+          <img className="phImage" src={item.image_url} alt={item.title} />
+          <div className="phContent">
+            <CardLink
+              link={item.url}
+              appendRef={false}
+              analyticsAttributes={{
+                [Attributes.POINTS]: item.image_url,
+                [Attributes.TRIGERED_FROM]: 'card',
+                [Attributes.TITLE]: item.title,
+                [Attributes.LINK]: item.url,
+                [Attributes.SOURCE]: 'producthunt',
+              }}>
+              {item.title}
+            </CardLink>
+            <p className="rowDescription">{item.description}</p>
+
+            {listingMode === 'normal' && (
+              <p className="rowDetails">
+                <span className="rowItem">
+                  <BiCommentDetail className="rowItemIcon" /> {item.comments || 0} comments
+                </span>
+                {item.tags && item.tags.length > 0 ? (
+                  <span className="rowItem">{item.tags[0]}</span>
+                ) : null}
+              </p>
+            )}
+          </div>
+          <div className="phVote">
+            <VscTriangleUp className="rowItemIcon" />
+            <span className="phVotesCount">{item.reactions}</span>
+          </div>
+        </div>
+      }
+    />
+  )
+}
+
+
+export default ArticleItem
