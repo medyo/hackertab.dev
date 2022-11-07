23999c92b3db6ff8e7eef30202f370645d578f32
medyo <elmehdi.sakout@gmail.com>
2022-11-07 23:44:08 +0100

    move keys to env

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -25,8 +25,9 @@ import {
 import { CardPropsType } from 'src/types'
 
 // Keys
-export const ANALYTICS_ENDPOINT = 'https://api.hackertab.dev/analytics'
-export const ANALYTICS_SDK_KEY = '9662c93f91473ba6e96711b22e0a367d'
+export const ANALYTICS_ENDPOINT = process.env.REACT_APP_AMPLITUDE_URL as string
+export const ANALYTICS_SDK_KEY = process.env.REACT_APP_AMPLITUDE_KEY as string
+export const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN as string
 export const LS_ANALYTICS_ID_KEY = 'hackerTabAnalyticsId'
 // Meta
 export const name = 'Hackertab.dev'
