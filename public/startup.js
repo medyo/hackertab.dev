0972b3674088c307b6f115de6280495cde1ffe80
medyo <elmehdi.sakout@gmail.com>
2023-01-10 20:26:05 +0100

    fix flicketing issue

--- public/startup.js
+++ public/startup.js
@@ -1,7 +1,7 @@
 // Blocking script to avoid the initial background flickering (switching from light to dark)
 // https://stackoverflow.com/a/63033934/3495717
 try {
-  var theme = JSON.parse(localStorage.hackerTabPrefs).theme || 'dark'
+  var theme = JSON.parse(localStorage.preferences_storage).theme || 'dark'
   document.documentElement.classList.add(theme)
 } catch (e) {
   console.log(e)
