f91760f9da3044bd98eec9c33fe0fc9bc89936d1
Mehdi Sakout <mehdi.sakout@journify.io>
2023-01-28 22:16:02 +0100

    skip the onboarding for old users

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useLayoutEffect, useState } from 'react'
 import 'react-contexify/dist/ReactContexify.css'
 import 'src/assets/App.css'
 import { Footer, Header } from 'src/components/Layout'
@@ -6,8 +6,10 @@ import { BookmarksSidebar } from 'src/features/bookmarks'
 import { MarketingBanner } from 'src/features/MarketingBanner'
 import { setupAnalytics, setupIdentification, trackPageView } from 'src/lib/analytics'
 import { useUserPreferences } from 'src/stores/preferences'
+import { diffBetweenTwoDatesInDays } from 'src/utils/DateUtils'
 import { AppContentLayout, ScrollCardsNavigator } from './components/Layout'
 import { isWebOrExtensionVersion } from './utils/Environment'
+import { getAppVersion } from './utils/Os'
 
 const OnboardingModal = React.lazy(() =>
   import('src/features/onboarding').then((module) => ({ default: module.OnboardingModal }))
@@ -17,7 +19,17 @@ function App() {
   const [showSideBar, setShowSideBar] = useState(false)
   const [showSettings, setShowSettings] = useState(false)
   const [showOnboarding, setShowOnboarding] = useState(true)
-  const { onboardingCompleted } = useUserPreferences()
+  const { onboardingCompleted, firstSeenDate, markOnboardingAsCompleted } = useUserPreferences()
+
+  useLayoutEffect(() => {
+    if (!onboardingCompleted && getAppVersion() <= '1.15.9') {
+      const usageDays = diffBetweenTwoDatesInDays(firstSeenDate, Date.now())
+      if (usageDays > 0) {
+        markOnboardingAsCompleted(null)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [onboardingCompleted, firstSeenDate])
 
   useEffect(() => {
     setupAnalytics()
