c4152b019d59136e809050fb957e96417f51e1bf
John Doe <elmehdi.sakout@gmail.com>
2023-02-18 13:52:03 +0100

    fix: displaying rss cards on mobile

--- src/components/Layout/MobileCards.tsx
+++ src/components/Layout/MobileCards.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { SUPPORTED_CARDS } from 'src/config'
 import { CustomRssCard } from 'src/features/cards'
+import { useUserPreferences } from 'src/stores/preferences'
 import { SelectedCard } from 'src/types'
 
 export const MobileCards = ({ selectedCard }: { selectedCard: SelectedCard }) => {
-  const currentCard = SUPPORTED_CARDS.find((c) => c.value === selectedCard.name)
+  const { userCustomCards } = useUserPreferences()
+  const currentCard = [...SUPPORTED_CARDS, ...userCustomCards].find(
+    (c) => c.value === selectedCard.name
+  )
+
   return currentCard
     ? React.createElement(currentCard?.component || CustomRssCard, {
         key: currentCard.value,
