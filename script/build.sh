190a6355552df3fe5d2695ed0cfe948f18852d2c
John Doe <elmehdi.sakout@gmail.com>
2023-11-19 15:54:38 +0100

    fix build script

--- script/build.sh
+++ script/build.sh
@@ -3,7 +3,7 @@
 build() {
     echo 'building react'
 
-    rm -rf dist/*
+    rm -rf dist
 
     export INLINE_RUNTIME_CHUNK=false
     export GENERATE_SOURCEMAP=false
@@ -11,7 +11,7 @@ build() {
     react-scripts build
 
     mkdir -p dist
-    cp -r build/* dist
+    cp -r build/* dist/
 }
 
 build
