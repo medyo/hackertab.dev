1ceae4084ec631cd31d493186ff4961df1be0309
Vineeth <vineeth.v.gooli@gmail.com>
2023-11-23 01:27:39 +0530

    Feature to share news/posts/articles (#162)
    * feature to share news/posts/articles
    
    * added title and source to share model
    
    * added title, removed redundancy of props
    
    * colorful icons & title prop added to button
    
    * css updated
    
    * css changes
    
    * fixed block-action-btns

--- src/components/Elements/CardWithActions/CardItemWithActions.tsx
+++ src/components/Elements/CardWithActions/CardItemWithActions.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { BiBookmarkMinus, BiBookmarkPlus } from 'react-icons/bi'
+import { BiBookmarkMinus, BiBookmarkPlus, BiShareAlt } from 'react-icons/bi'
+import { ShareModal } from 'src/features/shareModal'
 import { Attributes, trackLinkBookmark, trackLinkUnBookmark } from 'src/lib/analytics'
 import { useBookmarks } from 'src/stores/bookmarks'
 import { BaseEntry } from 'src/types'
@@ -19,6 +20,8 @@ export const CardItemWithActions = ({
   source,
   sourceType = 'supported',
 }: CardItemWithActionsProps) => {
+  const [showModal, setShowModal] = useState(false)
+  const [shareData, setShareData] = useState({})
   const { bookmarkPost, unbookmarkPost, userBookmarks } = useBookmarks()
   const [isBookmarked, setIsBookmarked] = useState(
     userBookmarks.some((bm) => bm.source === source && bm.url === item.url)
@@ -51,14 +54,26 @@ export const CardItemWithActions = ({
   useEffect(() => {
     setIsBookmarked(userBookmarks.some((bm) => bm.source === source && bm.url === item.url))
   }, [userBookmarks, source, item])
-
+  const handleOpenModal = () => {
+    setShowModal(!showModal)
+    setShareData({ title: item.title, link: item.url, source: source })
+  }
   return (
     <div key={`${source}-${index}`} className="blockRow">
+      <ShareModal showModal={showModal} setShowModal={handleOpenModal} shareData={shareData} />
+
       {cardItem}
       <div className={`blockActions ${isBookmarked ? 'active' : ''} `}>
         <button
+          className={`blockActionButton `}
+          onClick={handleOpenModal}
+          aria-label="Open share modal">
+          <BiShareAlt />
+        </button>
+        <button
           className={`blockActionButton ${isBookmarked ? 'active' : ''}`}
-          onClick={onBookmarkClick}>
+          onClick={onBookmarkClick}
+          aria-label="Bookmark item">
           {!isBookmarked ? <BiBookmarkPlus /> : <BiBookmarkMinus />}
         </button>
       </div>
---
+++ src/features/shareModal/components/ShareModal.tsx
@@ -0,0 +1,86 @@
+import { VscClose, VscCopy } from 'react-icons/vsc'
+import ReactModal from 'react-modal'
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TelegramIcon,
+  TelegramShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from 'react-share'
+import './share.css'
+
+type ShareModalProps = {
+  showModal: boolean
+  setShowModal: (show: boolean) => void
+  shareData: any
+}
+
+const sharingButtons = [
+  { component: EmailShareButton, icon: EmailIcon, title: 'Mail' },
+  { component: FacebookShareButton, icon: FacebookIcon, title: 'Facebook' },
+  { component: TwitterShareButton, icon: TwitterIcon, title: 'Twitter' },
+  { component: RedditShareButton, icon: RedditIcon, title: 'Reddit' },
+  { component: LinkedinShareButton, icon: LinkedinIcon, title: 'Linkedin' },
+  { component: TelegramShareButton, icon: TelegramIcon, title: 'Telegram' },
+  { component: WhatsappShareButton, icon: WhatsappIcon, title: 'Whatsapp' },
+]
+
+export const ShareModal = ({ showModal, setShowModal, shareData }: ShareModalProps) => {
+  const handleCloseModal = () => {
+    setShowModal(false)
+  }
+  const copyLink = () => {
+    navigator.clipboard.writeText(shareData.link)
+  }
+  return (
+    <ReactModal
+      isOpen={showModal}
+      ariaHideApp={true}
+      shouldCloseOnEsc={true}
+      shouldCloseOnOverlayClick={true}
+      shouldFocusAfterRender={false}
+      onRequestClose={() => handleCloseModal()}
+      className="shareModal "
+      style={{
+        overlay: {
+          zIndex: 3,
+        },
+      }}
+      overlayClassName="Overlay">
+      <div className="modalHeader">
+        <h1 className="modalTitle">Share</h1>
+        <button className="modalCloseBtn" onClick={handleCloseModal} aria-label="Close share modal">
+          <VscClose size="24" />
+        </button>
+      </div>
+
+      <div className="shareBody">
+        <p>{shareData.source}</p>
+        <p>{shareData.title}</p>
+        <div className="shareLink">
+          <input type="text" value={shareData.link} disabled />
+          <button onClick={copyLink} aria-label="Copy source url to clipboard">
+            <VscCopy />
+          </button>
+        </div>
+      </div>
+
+      <div className="shareOptions">
+        {sharingButtons.map(({ component: ShareButton, icon: Icon }, index) => (
+          <ShareButton key={index} url={shareData.link} title={shareData.title}>
+            <Icon size={32} round />
+          </ShareButton>
+        ))}
+      </div>
+    </ReactModal>
+  )
+}
