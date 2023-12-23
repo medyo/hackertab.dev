ea0b2df3e9102cb25aecdcb58ec2b83f529931d1
John Doe <elmehdi.sakout@gmail.com>
2023-12-23 15:32:57 +0100

    optimize the build

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,14 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const buildTarget = env.VITE_BUILD_TARGET || 'web'
+  const buildPlatform = env.VITE_BUILD_PLATFORM
 
   if (!env.VITE_API_URL) {
     throw new Error('VITE_API_URL is not defined, create an .env file with this variable')
+  }
+
+  if (buildTarget == 'extension' && !buildPlatform) {
+    throw new Error('VITE_BUILD_PLATFORM is not defined, create an .env file with this variable')
   }
 
   return {
@@ -29,14 +34,8 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       emptyOutDir: true,
-      minify: 'terser',
-      terserOptions: {
-        format: {
-          beautify: true,
-        },
-      },
       rollupOptions: {
-        ...(buildTarget === 'extension'
+        ...(buildTarget === 'extension' && buildPlatform === 'firefox'
           ? {
               output: {
                 manualChunks: () => 'main.js',
@@ -45,7 +44,23 @@ export default defineConfig(({ mode }) => {
                 assetFileNames: `assets/[name].[ext]`,
               },
             }
-          : {}),
+          : {
+              output: {
+                manualChunks: {
+                  settings: [
+                    'react-contexify',
+                    'react-modal',
+                    'react-select',
+                    'react-share',
+                    'react-simple-toasts',
+                    'react-spinners',
+                    'react-toggle',
+                    'react-tooltip',
+                  ],
+                  react: ['react', 'react-dom', 'react-router-dom', 'react-device-detect'],
+                },
+              },
+            }),
       },
     },
     server: {
