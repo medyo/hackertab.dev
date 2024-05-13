806da413d3e80a8b52fab814f1058e9831f50f88
John Doe <elmehdi.sakout@gmail.com>
2024-05-13 19:48:27 +0100

    remove unused references

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,13 +1,3 @@
-import { ReactComponent as BaiduLogo } from 'src/assets/baidu_logo.svg'
-import { ReactComponent as BingLogo } from 'src/assets/bing_logo.svg'
-import { ReactComponent as DuckDuckGoLogo } from 'src/assets/duckduckgo_logo.svg'
-import { ReactComponent as GoogleLogo } from 'src/assets/google_logo.svg'
-import { ReactComponent as PhindLogo } from 'src/assets/phind_logo.svg'
-import { ReactComponent as StartPageLogo } from 'src/assets/startpage_logo.svg'
-import { ReactComponent as YahooLogo } from 'src/assets/yahoo_logo.svg'
-import { ReactComponent as YandexLogo } from 'src/assets/yandex_logo.svg'
-import { ReactComponent as KagiLogo} from 'src/assets/kagi_logo.svg'
-
 // Keys
 export const ANALYTICS_ENDPOINT = import.meta.env.VITE_AMPLITUDE_URL as string
 export const ANALYTICS_SDK_KEY = import.meta.env.VITE_AMPLITUDE_KEY as string
@@ -27,56 +17,6 @@ export const dataSourcesLink = 'https://www.hackertab.dev/data-sources'
 export const changeLogLink = 'https://api.github.com/repos/medyo/hackertab.dev/releases'
 export const twitterHandle = '@hackertabdev'
 export const reportLink = 'https://www.hackertab.dev/report'
-// Cfgs
-export const SUPPORTED_SEARCH_ENGINES = [
-  {
-    label: 'Google',
-    url: 'https://google.com/search?q=',
-    logo: GoogleLogo,
-  },
-  {
-    label: 'DuckDuckGo',
-    url: 'https://duckduckgo.com?q=',
-    logo: DuckDuckGoLogo,
-  },
-  {
-    label: 'Bing',
-    url: 'https://bing.com/search?q=',
-    logo: BingLogo,
-  },
-  {
-    label: 'Yahoo',
-    url: 'https://search.yahoo.com/search?p=',
-    logo: YahooLogo,
-  },
-  {
-    label: 'Baidu',
-    url: 'https://baidu.com/s?wd=',
-    logo: BaiduLogo,
-  },
-  {
-    label: 'Yandex',
-    url: 'https://yandex.ru/search/?text=',
-    logo: YandexLogo,
-    className: 'themeAdaptiveFillColor',
-  },
-  {
-    label: 'Startpage',
-    url: 'https://www.startpage.com/sp/search?query=',
-    logo: StartPageLogo,
-  },
-  {
-    label: 'Phind',
-    url: 'https://phind.com/search?q=',
-    logo: PhindLogo,
-    className: 'themeAdaptiveFillColor',
-  },
-  {
-    label: 'Kagi',
-    url: 'https://kagi.com/search?q=',
-    logo: KagiLogo,
-  },
-]
 
 export const LS_PREFERENCES_KEY = 'hackerTabPrefs'
 export const GLOBAL_TAG = {
