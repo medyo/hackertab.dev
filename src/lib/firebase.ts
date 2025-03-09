2d4d63852967a405a8020e3023dd71d907fddb21
John Doe <elmehdi.sakout@gmail.com>
2025-03-09 15:26:05 +0000

    feat: Simplify Firebase configuration by removing unused keys and adding build target

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,35 +1,13 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth'
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_APP_ID,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_MEASUREMENT_ID,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-} from 'src/config'
+import { FIREBASE_API_KEY } from 'src/config'
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
 }
 
-if (
-  !FIREBASE_API_KEY ||
-  !FIREBASE_APP_ID ||
-  !FIREBASE_AUTH_DOMAIN ||
-  !FIREBASE_MEASUREMENT_ID ||
-  !FIREBASE_MESSAGING_SENDER_ID ||
-  !FIREBASE_PROJECT_ID ||
-  !FIREBASE_STORAGE_BUCKET
-) {
-  console.warn('Missing Firebase configuration')
+if (!FIREBASE_API_KEY) {
+  console.warn('Missing Firebase api Key')
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
