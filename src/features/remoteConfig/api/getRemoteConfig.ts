bd4ed51076310403f464142fab6b8c5bf4f4e899
John Doe <elmehdi.sakout@gmail.com>
2025-04-07 17:36:04 +0100

    refactor: standardize import statements and remove unnecessary line breaks in getRemoteConfig

--- src/features/remoteConfig/api/getRemoteConfig.ts
+++ src/features/remoteConfig/api/getRemoteConfig.ts
@@ -1,25 +1,25 @@
-import { useQuery } from '@tanstack/react-query';
-import { ExtractFnReturnType, QueryConfig } from 'src/lib/react-query';
-import { RemoteConfig } from "../types";
-import { axios } from 'src/lib/axios';
-import { useRemoteConfigStore } from "../stores/remoteConfig";
+import { useQuery } from '@tanstack/react-query'
+import { axios } from 'src/lib/axios'
+import { ExtractFnReturnType, QueryConfig } from 'src/lib/react-query'
+import { useRemoteConfigStore } from '../stores/remoteConfig'
+import { RemoteConfig } from '../types'
 
 const getRemoteConfig = async (): Promise<RemoteConfig> => {
-  return axios.get('/data/remoteConfiguration.json');
+  return axios.get('/data/remoteConfiguration.json')
 }
 
-type QueryFnType = typeof getRemoteConfig;
+type QueryFnType = typeof getRemoteConfig
 
 type UseGetRemoteConfigOptions = {
-  config?: QueryConfig<QueryFnType>;
-};
+  config?: QueryConfig<QueryFnType>
+}
 export const useGetRemoteConfig = ({ config }: UseGetRemoteConfigOptions = {}) => {
   return useQuery<ExtractFnReturnType<QueryFnType>>({
     onSuccess(remoteConfig) {
-      useRemoteConfigStore.getState().setRemoteConfig(remoteConfig);
+      useRemoteConfigStore.getState().setRemoteConfig(remoteConfig)
     },
     ...config,
     queryKey: ['remote-config'],
     queryFn: () => getRemoteConfig(),
-  });
+  })
 }
