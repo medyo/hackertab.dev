f89655e3490709a72d23af07b584d05278e11d94
John Doe <elmehdi.sakout@gmail.com>
2025-06-25 23:08:38 +0100

    refactor: replace useGetAIArticles with useGetFeed and update rendering logic in AICard component

--- src/features/cards/components/aiCard/AICard.tsx
+++ src/features/cards/components/aiCard/AICard.tsx
@@ -1,21 +1,16 @@
 import { Card } from 'src/components/Elements'
 import { ListComponent } from 'src/components/List'
-import { useFeatureFlags } from 'src/stores/featureFlags'
+import { FeedItem, useGetFeed } from 'src/features/feed'
 import { useUserPreferences } from 'src/stores/preferences'
-import { CardPropsType, FeedItem } from 'src/types'
-import { useGetAIArticles } from '../../api/getAIArticles'
-import ArticleItem from './ArticleItem'
-
-const SHOW_PANEL_FEATURE_FLAG = 'ai_panel_shown'
+import { CardPropsType, FeedItemData } from 'src/types'
 
 export function AICard({ meta, withAds }: CardPropsType) {
   const { userSelectedTags } = useUserPreferences()
-  const { isDone, markDone } = useFeatureFlags()
   const {
     data: articles,
     isLoading,
     error,
-  } = useGetAIArticles({
+  } = useGetFeed({
     tags: userSelectedTags.map((tag) => tag.label.toLocaleLowerCase()),
     config: {
       cacheTime: 0,
@@ -24,13 +19,13 @@ export function AICard({ meta, withAds }: CardPropsType) {
     },
   })
 
-  const renderItem = (item: FeedItem, index: number) => (
-    <ArticleItem item={item} key={`ai-${index}`} index={index} analyticsTag={meta.analyticsTag} />
+  const renderItem = (item: FeedItemData, index: number) => (
+    <FeedItem item={item} key={`ai-${index}`} index={index} analyticsTag={meta.analyticsTag} />
   )
 
   return (
     <Card card={meta} withAds={withAds}>
-      <ListComponent<FeedItem>
+      <ListComponent<FeedItemData>
         items={articles?.pages.flatMap((page) => page.data) || []}
         error={error}
         isLoading={isLoading}
