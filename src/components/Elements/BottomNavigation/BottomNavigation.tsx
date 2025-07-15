78f23c467227e9cad5c129b6189dd16bd3548fd4
John Doe <elmehdi.sakout@gmail.com>
2025-07-15 21:29:05 +0100

    fix: remove unused imports and clean up BottomNavigation component

--- src/components/Elements/BottomNavigation/BottomNavigation.tsx
+++ src/components/Elements/BottomNavigation/BottomNavigation.tsx
@@ -1,6 +1,4 @@
-import { AiOutlineMenu } from 'react-icons/ai'
 import { BsRssFill } from 'react-icons/bs'
-import { useNavigate } from 'react-router-dom'
 import { SUPPORTED_CARDS } from 'src/config/supportedCards'
 import { useUserPreferences } from 'src/stores/preferences'
 import { SelectedCard } from 'src/types'
@@ -13,8 +11,6 @@ type BottomNavigationProps = {
 export const BottomNavigation = ({ selectedCard, setSelectedCard }: BottomNavigationProps) => {
   const { cards, userCustomCards } = useUserPreferences()
   const AVAILABLE_CARDS = [...SUPPORTED_CARDS, ...userCustomCards]
-  const navigate = useNavigate()
-
   return (
     <div className="bottomNavigation">
       {cards.map((card) => {
@@ -29,20 +25,10 @@ export const BottomNavigation = ({ selectedCard, setSelectedCard }: BottomNaviga
             onClick={() => setSelectedCard(card)}>
             {card.type === 'supported'
               ? constantCard?.icon
-              : <img src={constantCard?.icon as string} alt="" /> || <BsRssFill className="rss" />}
+              : <img src={constantCard?.icon} alt="" /> || <BsRssFill className="rss" />}
           </button>
         )
       })}
-      {
-        <button
-          aria-label="Open settings"
-          className={'navigationItem '}
-          onClick={() => {
-            navigate('/settings/general')
-          }}>
-          {<AiOutlineMenu />}
-        </button>
-      }
     </div>
   )
 }
