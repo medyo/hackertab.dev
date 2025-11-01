ae2d11c0b1ac7908d0e8267c9d8e836f42ceba8e
John Doe <elmehdi.sakout@gmail.com>
2025-11-01 12:59:58 +0100

    refactor: simplify RemoteConfigStore by removing unused properties and configurations

--- src/features/remoteConfig/stores/remoteConfig.ts
+++ src/features/remoteConfig/stores/remoteConfig.ts
@@ -4,47 +4,24 @@ import { persist } from 'zustand/middleware'
 import { RemoteConfig, Tag } from '../types'
 
 type RemoteConfigStore = {
-  supportedTags: Tag[]
-  marketingBannerConfig?: any
-  adsConfig: {
-    rowPosition: number
-    columnPosition: number
-    enabled: boolean
-  }
+  tags: Tag[]
   setRemoteConfig: (remoteConfig: RemoteConfig) => void
 }
 
 export const useRemoteConfigStore = create(
   persist<RemoteConfigStore>(
     (set) => ({
-      marketingBannerConfig: undefined,
-      adsConfig: {
-        rowPosition: 0,
-        columnPosition: 0,
-        enabled: false,
-      },
-      supportedTags: [
+      tags: [
         {
           value: 'javascript',
           label: 'Javascript',
         },
       ],
-      setRemoteConfig: (remoteConfig: RemoteConfig) =>
-        set(() => {
-          const { marketingBannerConfig, ...otherConfigs } = remoteConfig
-          return { ...otherConfigs }
-        }),
+      setRemoteConfig: (remoteConfig: RemoteConfig) => set({ tags: remoteConfig.tags }),
     }),
     {
       name: 'remote_config_storage',
-      version: 1,
-      migrate(persistedState, version) {
-        const newState = persistedState as RemoteConfigStore
-        if (version === 0) {
-          delete newState.marketingBannerConfig
-        }
-        return newState
-      },
+      version: 2,
     }
   )
 )
