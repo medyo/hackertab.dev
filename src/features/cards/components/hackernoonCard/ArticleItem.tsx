7cb791c4ff625bb700b180410c4d7964e02161cb
John Doe <elmehdi.sakout@gmail.com>
2025-11-22 20:30:13 +0100

    feat: add HackernoonCard component and associated ArticleItem with icon

---
+++ src/features/cards/components/hackernoonCard/ArticleItem.tsx
@@ -0,0 +1,47 @@
+import { MdAccessTime } from 'react-icons/md'
+import { CardItemWithActions, CardLink, ColoredLanguagesBadge } from 'src/components/Elements'
+import { Attributes } from 'src/lib/analytics'
+import { useUserPreferences } from 'src/stores/preferences'
+import { Article, BaseItemPropsType } from 'src/types'
+import { format } from 'timeago.js'
+
+const ArticleItem = (props: BaseItemPropsType<Article>) => {
+  const { item, selectedTag, analyticsTag } = props
+  const { listingMode } = useUserPreferences()
+  return (
+    <CardItemWithActions
+      source={analyticsTag}
+      item={item}
+      cardItem={
+        <>
+          <CardLink
+            link={item.url}
+            analyticsAttributes={{
+              [Attributes.TRIGERED_FROM]: 'card',
+              [Attributes.TITLE]: item.title,
+              [Attributes.LINK]: item.url,
+              [Attributes.SOURCE]: analyticsTag,
+              [Attributes.LANGUAGE]: selectedTag?.value,
+            }}>
+            <div className="subTitle">{item.title}</div>
+          </CardLink>
+          {listingMode === 'normal' && (
+            <>
+              <p className="rowDescription">
+                <span className="rowItem">
+                  <MdAccessTime className={'rowTitleIcon'} />
+                  {format(new Date(item.published_at))}
+                </span>
+              </p>
+              <p className="rowDetails">
+                <ColoredLanguagesBadge languages={item.tags.slice(0, 4)} />
+              </p>
+            </>
+          )}
+        </>
+      }
+    />
+  )
+}
+
+export default ArticleItem
---
+++ src/features/cards/components/hackernoonCard/HackernoonCard.tsx
@@ -0,0 +1,64 @@
+import { useCallback } from 'react'
+import { Card } from 'src/components/Elements'
+import { ListPostComponent } from 'src/components/List/ListPostComponent'
+import { Article, CardPropsType } from 'src/types'
+import { useGetSourceArticles } from '../../api/getSourceArticles'
+import { useLazyListLoad } from '../../hooks/useLazyListLoad'
+import { useSelectedTags } from '../../hooks/useSelectedTags'
+import { MemoizedCardHeader } from '../CardHeader'
+import { MemoizedCardSettings } from '../CardSettings'
+import ArticleItem from './ArticleItem'
+
+const GLOBAL_TAG = { label: 'Global', value: '' }
+
+export function HackernoonCard(props: CardPropsType) {
+  const { meta } = props
+  const { ref, isVisible } = useLazyListLoad()
+  const {
+    queryTags,
+    selectedTag,
+    cardSettings: { sortBy, language } = {},
+  } = useSelectedTags({
+    source: meta.value,
+    fallbackTag: GLOBAL_TAG,
+  })
+
+  const { data, isLoading } = useGetSourceArticles({
+    source: 'hackernoon',
+    tags: queryTags,
+    config: {
+      enabled: isVisible,
+    },
+  })
+
+  const renderItem = useCallback(
+    (item: Article) => <ArticleItem item={item} key={item.id} analyticsTag={meta.analyticsTag} />,
+    [meta.analyticsTag]
+  )
+
+  return (
+    <Card
+      ref={ref}
+      titleComponent={
+        <MemoizedCardHeader label={meta.label} fallbackTag={GLOBAL_TAG} selectedTag={selectedTag} />
+      }
+      settingsComponent={
+        <MemoizedCardSettings
+          url={meta.link}
+          id={meta.value}
+          globalTag={GLOBAL_TAG}
+          sortBy={sortBy}
+          language={language || GLOBAL_TAG.value}
+          showDateRangeFilter={false}
+        />
+      }
+      {...props}>
+      <ListPostComponent
+        sortBy={sortBy as keyof Article}
+        items={data}
+        isLoading={isLoading}
+        renderItem={renderItem}
+      />
+    </Card>
+  )
+}
