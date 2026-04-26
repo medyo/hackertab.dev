7c8b54ae14dbe140a7a84b1c533dfa2a3cb00b74
John Doe <elmehdi.sakout@gmail.com>
2026-04-26 23:10:58 +0100

    fix: update getAd function to return Ad[] and adjust AdInitializer to handle multiple ads

--- src/features/adv/api/getAd.ts
+++ src/features/adv/api/getAd.ts
@@ -1,11 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { axios } from 'src/lib/axios'
 import { ExtractFnReturnType, QueryConfig } from 'src/lib/react-query'
-import { Ad } from '../types'
 
-const getAd = async (keywords: string[], feed: boolean = false): Promise<Ad | null> => {
+const getAd = async (keywords: string[], feed: boolean = false): Promise<Ad[]> => {
   let params = { keywords: keywords.join(','), feed: feed ? 'true' : 'false' }
-  return axios.get('/engine/ads/adaptive', { params })
+  return axios.get('/engine/ads/adaptive_v2', { params })
 }
 
 type QueryFnType = typeof getAd
@@ -18,7 +17,7 @@ type UseGetAdOptions = {
 export const useGetAd = ({ keywords, feed, config }: UseGetAdOptions) => {
   return useQuery<ExtractFnReturnType<QueryFnType>>({
     ...config,
-    queryKey: ['ad', 'v2', keywords.join(',')],
+    queryKey: ['ad', 'v3', keywords.join(',')],
     queryFn: () => getAd(keywords, feed),
   })
 }
--- src/features/adv/providers/AdInitializer.tsx
+++ src/features/adv/providers/AdInitializer.tsx
@@ -8,7 +8,7 @@ export const AdInitializer = () => {
   const { setAds, setIsLoading } = useAdStore()
   const {
     isSuccess,
-    data: ad,
+    data: advs,
     isLoading,
   } = useGetAd({
     keywords: userSelectedTags.map((tag) => tag.label.toLocaleLowerCase()),
@@ -25,51 +25,10 @@ export const AdInitializer = () => {
   }, [isLoading])
 
   useEffect(() => {
-    if (isSuccess && ad) {
-      setAds([
-        /*{
-          type: 'large-img',
-          id: '1',
-          title: 'Hackertab Pro',
-          description: 'Upgrade to Hackertab Pro for an ad-free experience and exclusive features.',
-          link: 'https://hackertab.dev/pro',
-          imageUrl: 'https://cdn.hackertab.dev/assets/house-ad-banner.jpg',
-        },*/
-        {
-          type: 'small-img',
-          id: '1',
-          title: 'Hackertab Pro',
-          cta_text: 'Try for free',
-          logoUrl: 'https://www.coderabbit.ai/images/logo-dark.svg',
-          style: {
-            bg_color: '#000',
-            text_color: '#FFFFFF',
-            cta_bg_color: '#FFFFFF',
-            cta_text_color: '#000000',
-          },
-          description: 'Cut code review time & bugs in half, instantly.',
-          link: 'https://www.coderabbit.ai/',
-          imageUrl: 'https://cdn.hackertab.dev/assets/house-ad-banner.jpg',
-        },
-        {
-          type: 'sticky-ad',
-          id: '1',
-          title: 'Cut code review time & bugs in half, instantly.',
-          cta_text: 'Try for free',
-          style: {
-            bg_color: '#000',
-            text_color: '#FFFFFF',
-            cta_bg_color: '#FFFFFF',
-            cta_text_color: '#000000',
-          },
-          condition: '$[?(@.platform=="extension")]',
-          link: 'https://www.coderabbit.ai/',
-          imageUrl:
-            'https://www.coderabbit.ai/images/logo-dark.svg?dpl=dpl_4EGaXfdbadu5Dvw7KvvswknjNbQ6',
-        },
-      ])
+    if (isSuccess && advs) {
+      setAds(advs)
     }
-  }, [ad])
+  }, [advs, isSuccess])
 
   return null
 }
