37c07b80c60952ee81ccc87bb039ade0a53296b1
John Doe <elmehdi.sakout@gmail.com>
2026-04-26 23:10:46 +0100

    refactor: reorganize ad types and remove unused definitions

--- src/features/adv/stores/AdStore.tsx
+++ src/features/adv/stores/AdStore.tsx
@@ -1,46 +1,5 @@
 import { create } from 'zustand'
 
-type CommonAdFields = {
-  id: string
-  link: string
-  sponsored_by?: string
-}
-
-type AdStyle = {
-  bg_color: string
-  text_color: string
-  cta_bg_color?: string
-  cta_text_color?: string
-}
-type SmallImageAd = CommonAdFields & {
-  type: 'small-img'
-  title: string
-  description: string
-  logoUrl?: string
-  imageUrl: string
-  cta_text: string
-  style: AdStyle
-}
-
-type LargeImageAd = CommonAdFields & {
-  type: 'large-img'
-  title: string
-  description: string
-  link: string
-  imageUrl: string
-}
-
-type StickyAd = CommonAdFields & {
-  type: 'sticky-ad'
-  title: string
-  imageUrl: string
-  condition?: string
-  cta_text: string
-  style: AdStyle
-  dismissible?: boolean
-}
-
-type Ad = SmallImageAd | LargeImageAd | StickyAd
 type AdStore = {
   ads: Ad[]
   isLoading: boolean
